36632e9b648bf77c083b37ec359e1268c7fcb4d5
T4ngoo <tomas.carvas@gmail.com>
2025-02-06 17:48:59 +0100

    Add function to calculate estimate, a ratio of real/modeled price

--- main_search.py
+++ main_search.py
@@ -9,7 +9,8 @@ from src.annuity_processor import AnnuityProcessor
 annuity_processor = AnnuityProcessor()
 from src.searcher import Searcher
 searcher = Searcher()
-
+from src.scraper import Scraper
+scraper = Scraper()
 
 if __name__ == "__main__":
 
@@ -22,22 +23,17 @@ if __name__ == "__main__":
 
 
     if data_type == 'sale':
-        # TODO add scraper SALE
+        scraper.run(intent="sale")
         data_processor.process_data(process_sale=True, process_rent=False, search = True)
         annuity_processor.process_data_annuity(search = True)
         result_df = searcher.search_apartments(process_sale=True, process_rent=False)
-        # TODO add concatenate of data + predictions
+        scraper.get_estimates(estimates=result_df, intent="sale")
 
     elif data_type == 'rent':
-        # TODO add scraper RENT
+        scraper.run(intent="rent")
         data_processor.process_data(process_sale=False, process_rent=True, search = True)
         result_df = searcher.search_apartments(process_sale=False, process_rent=True)
-        # TODO add concatenate of data + predictions
-
-
-
-    print(result_df)
-    
+        scraper.get_estimates(estimates=result_df, intent="rent")
 
 
 
@@ -80,39 +76,3 @@ bools (true | false):   "balcony="
                         "elevator="
 ---------------------------------------------------------
 """
-
-
-from src.data_processor import DataProcessor
-data_processor = DataProcessor()
-from src.annuity_processor import AnnuityProcessor
-annuity_processor = AnnuityProcessor()
-from src.searcher import Searcher
-searcher = Searcher()
-
-
-if __name__ == "__main__":
-
-
-    # -----------------------------------------------------------------------
-    # DECIDE WHETHER YOU WANT TO BUY OR RENT AN APARTMENT
-
-    data_type = 'sale'            # Choose: 'sale' | 'rent'
-    # -----------------------------------------------------------------------
-
-
-    if data_type == 'sale':
-        # TODO add scraper SALE
-        data_processor.process_data(process_sale=True, process_rent=False, search = True)
-        annuity_processor.process_data_annuity(search = True)
-        result_df = searcher.search_apartments(process_sale=True, process_rent=False)
-    elif data_type == 'rent':
-        # TODO add scraper RENT
-        data_processor.process_data(process_sale=False, process_rent=True, search = True)
-        result_df = searcher.search_apartments(process_sale=False, process_rent=True)
-
-    print(result_df)
-    
-
-
-
-
